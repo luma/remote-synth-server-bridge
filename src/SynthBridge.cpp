634a2dc95a057dfdb4fdabe03790987406c3ba11
Rolly Fordham <rolly@luma.co.nz>
2015-05-19 21:33:51 +1000

    various fixes

--- src/SynthBridge.cpp
+++ src/SynthBridge.cpp
@@ -1,7 +1,47 @@
 #include <node.h>
+
+#include "base/logging.h"
+#include "base/ssladapter.h"
+
 #include "PeerWrapper.h"
 
 using namespace v8;
+
+static const char *fieldTrials = NULL;
+
+void SetupWebRtc() {
+  webrtc::field_trial::InitFieldTrialsFromString(fieldTrials);
+
+  if (!rtc::InitializeSSL(NULL)) {
+    // @todo log error "Failed to initialise SSL";
+    // @todo something?
+    return;
+  }
+  else {
+    // @todo log info "Initialised SSL";
+  }
+
+  if (!rtc::InitializeSSLThread()) {
+    // @todo log error "Failed to initialise SSL Thread";
+    // @todo something?
+    return;
+  }
+  else {
+    // @todo log info "Initialised SSL Thread";
+  }
+}
+
+// @todo call this
+void TearDownWebRtc() {
+  if (!rtc::CleanupSSL()) {
+    // @todo log error "Failed to successfully clean up SSL";
+  }
+  else {
+    // @todo log info "Cleaned up SSL";
+  }
+}
+
+
 
 void CreatePeer(const FunctionCallbackInfo<Value>& args) {
   Isolate* isolate = Isolate::GetCurrent();
@@ -10,6 +50,8 @@ void CreatePeer(const FunctionCallbackInfo<Value>& args) {
 }
 
 void InitAll(Handle<Object> exports) {
+  SetupWebRtc();
+
   PeerWrapper::Init(exports);
 
   NODE_SET_METHOD(exports, "Peer", CreatePeer);
